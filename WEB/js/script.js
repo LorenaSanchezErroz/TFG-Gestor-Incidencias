45a936196223c88a5dd3a9353c8643b438beb103
Lorens Sanchez Erroz <lorena.sanchez744@comunidadunir.net>
2025-11-28 17:13:23 +0100

    "UPDATE:  index.html, style.css, script.js, otros.html"

--- WEB/js/script.js
+++ WEB/js/script.js
@@ -1,3 +1,12 @@
+ // NO MOVER DE AQUÍ ESTA FUNCIÓN, SI NO NO FUNCIONA
+ // redirección a la página web de la lavandería al pulsar el icono 
+ const logo = document.querySelector(".icono"); // logoripo de boreas
+ logo.addEventListener('click', () => {
+    window.location.href = "https://www.lavanderiaboreas.com/"
+ });
+
+ 
+ 
  // Selección de elementos (input e iconos)
 
  const inputPass = document.getElementById("password")
